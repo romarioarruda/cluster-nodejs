a761dfbb13837da2f95f18bc2277d8e9719ba06d
Romário Arruda <romarioarruda98@gmail.com>
2022-05-24 16:53:52 -0300

    feat: gerenciando erros não tratados com o gerenciado de processos do node

--- cluster/index.js
+++ cluster/index.js
@@ -10,7 +10,7 @@ function runMasterProcess() {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
+  cluster.on('exit', (worker, code, _signal) => {
     if(code !== 0 && !worker.exitedAfterDisconnect) {
       console.log(`worker ${worker.process.pid} died, scheduling new one!`);
       cluster.fork();
@@ -31,3 +31,15 @@ function runWorkerProcess() {
 
 
 cluster.isMaster ? runMasterProcess() : runWorkerProcess()
+
+
+//Captura erros não tratados.
+//Sem isso, o sistema quebra. Se não tiver algo monitorando o processo e subindo um novo.
+process.on('uncaughtException', (error, origin) => {
+  console.log(`\n${origin} signal received: \n ${error}`)
+})
+
+//Sem isso, o sistema lança um warning.
+process.on('unhandledRejection', (error) => {
+  console.log(`\n unhandledRejection signal received: \n ${error}`)
+})
