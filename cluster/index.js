0abf789beb2b7bf7bf9bb79518f9c65c90d5e0b3
Romário Arruda <romarioarruda98@gmail.com>
2022-05-24 16:11:50 -0300

    refact: refatorando codigo em funcoes separadas

--- cluster/index.js
+++ cluster/index.js
@@ -1,28 +1,33 @@
 const cluster = require('cluster');
 const http = require('http');
-const numCPUs = require('os').cpus().length;
+const numCPUs = require('os').cpus().length * 2;
+const fibonacci = require('./fibonacci')
 
-const fibonacci = (n) => {
-    return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
-}
-
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
+function runMasterProcess() {
+  console.log(`Master ${process.pid} is running with ${numCPUs} processes`);
+  // creating processes according my cpu number
   for (let i = 1; i < numCPUs; i++) {
     cluster.fork();
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+    if(code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(`worker ${worker.process.pid} died, scheduling new one!`);
+      cluster.fork();
+    }
   });
-} else {
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    const result = fibonacci(43)
-    res.end(`Resultado calculo: ${result}`);
-  }).listen(8000);
-
-  console.log(`Worker ${process.pid} started, CPUS: ${numCPUs}`);
 }
+
+
+function runWorkerProcess() {
+  http.createServer((_req, res) => {
+    res.writeHead(200);
+    const result = fibonacci(20)
+    res.end(`Resultado calculo: ${result}`);
+  }).listen(8000)
+
+  console.log(`Worker ${process.pid} started`);
+}
+
+
+cluster.isMaster ? runMasterProcess() : runWorkerProcess()
